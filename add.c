c6dabe8f51230292673489d11aceef8abeda71d2
Alwin203 <alwinbhandari303@gmail.com>
2024-01-16 10:06:39 +0545

    ADD

---
+++ add.c
@@ -0,0 +1,13 @@
+//WAP to ADD two numbers
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter first number\n");
+    scanf("%d",&a);
+    printf("enter second number\n");
+    scanf("%d",&b);
+    sum = a+b;
+    printf("The sum of %d and %d is %d",a,b,sum);
+    return 0;
+}
