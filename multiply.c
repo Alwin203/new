fee22789dcae31be91f173cbdaa0faaf8b6059e0
Alwin203 <alwinbhandari303@gmail.com>
2024-01-17 17:35:47 +0545

    multiply

---
+++ multiply.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,mul;
+    printf("enter first number :\n");
+    scanf("%d",&a);
+    printf("enter second number :\n");
+    scanf("%d",&b);
+    mul=a*b;
+    printf("Result of multlipication between %d and %d is %d",a,b,mul);
+    return 0;
+}
