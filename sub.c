04521b5677df1a6f0de312e110c2dcf99e582a9b
Alwin203 <alwinbhandari303@gmail.com>
2024-01-19 15:55:34 +0545

    subtraction

---
+++ sub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a , b , sub1,sub2;
+printf("enter a number \n");
+scanf("%d",&a);
+printf("enter b number \n");
+scanf("%d",&b);
+sub1 = a-b;
+sub2 = b-a;
+printf("%d - %d = %d \n %d - %d = %d",a,b,sub1,a,b,sub2);
+    return 0;
+}
