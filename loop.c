9fdcce1bb7321e6f112d9ec931d3144b89b87d48
Alwin203 <alwinbhandari303@gmail.com>
2024-01-21 19:57:20 +0545

    loop

---
+++ loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter how many times u want to print Hello world\n");
+    scanf("%d",&a);
+    for(b=0;a>b;b++)
+    {
+    printf("Hello World\n");
+    }
+    return 0;
+}
