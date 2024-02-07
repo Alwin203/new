2f87ce53b82232774778a5600624a158c05516dd
Alwin203 <alwinbhandari303@gmail.com>
2024-02-07 21:04:23 +0545

    Avg

---
+++ sumandavg.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,sum,avg;
+    printf("Enter three numbers");
+    scanf("%d%d%d",&a,&b,&c);
+    sum=a+b+c;
+    avg=(a+b+c)/3;
+    printf("Sum : %d\nAvg:%d",sum,avg);
+    return 0;
+}
